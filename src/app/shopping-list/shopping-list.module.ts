f4678c5082a0fc31d9798a824049c466f9bee677
beorn <beorn_d_latch@yahoo.com>
2018-08-17 02:12:18 +0200

    Added splitting in modules

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from "@angular/core";
 import {ShoppingListComponent} from "./shopping-list.component";
 import {ShoppingEditComponent} from "./shopping-edit/shopping-edit.component";
 import {SharedModule} from "../shared/shared.module";
-import {ShoppingListRoutingModule} from "./shopping-list-routing.module";
 import {FormsModule} from "@angular/forms";
 
 @NgModule({
@@ -12,7 +11,6 @@ import {FormsModule} from "@angular/forms";
   ],
   imports : [
     FormsModule,
-    ShoppingListRoutingModule,
     SharedModule
   ]
 })
